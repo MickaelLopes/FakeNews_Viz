905a3656d43c8b5b4df009f1b9829ba47551dbc3
Mickael Lopes <mlopes334@beijaflore.com>
2020-02-16 09:25:02 +1100

    update db file

--- db.py
+++ db.py
@@ -2,6 +2,8 @@ import pymongo
 from bson.json_util import dumps
 import json
 
+news_id = 13775
+
 mongo = pymongo.MongoClient('mongodb+srv://lopesmickael8:C1348m13@cluster0-mqdrm.mongodb.net/test?retryWrites=true&w=majority', maxPoolSize=50, connect=False)
 
 
@@ -9,7 +11,15 @@ db = pymongo.database.Database(mongo, 'FakeNews')
 news_col = pymongo.collection.Collection(db, 'News')
 tweets_col = pymongo.collection.Collection(db, 'Tweets')
 
-col_results = json.loads(dumps(news_col.find_one({"_id":13806})))
-tweets_col = json.loads(dumps(tweets_col.find_one({"_id":13806})))
+col_results = json.loads(dumps(news_col.find_one({"_id":news_id})))
+tweets_col = json.loads(dumps(tweets_col.find_one({"_id":news_id})))
+
 
 print(tweets_col)
+
+# When working in database connection - save file to be loaded in main.js
+with open(f'{news_id}.json', 'w') as json_file:
+    json.dump(col_results, json_file)
+
+with open(f't_{news_id}.json', 'w') as json_file2:
+    json.dump(tweets_col, json_file2)
